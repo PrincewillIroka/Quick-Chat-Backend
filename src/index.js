efa48d6834c78cda8c88c80b2fe22b86f601f0df
Princewill Iroka <princewilliroka@outlook.com>
2023-05-28 01:27:52 +0100

    Setup routes and controllers

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ require("dotenv").config();
 
 const app = express();
 
+const routes = require("./routes");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.APP_PORT;
+
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Connected on port ${port}...`);
