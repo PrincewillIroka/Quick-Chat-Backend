f75f9d19bb3e4f605dafebf293d12bfc461cbca6
Princewill Iroka <princewilliroka@outlook.com>
2023-05-31 01:57:53 +0100

    Updated User controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,19 @@
 import Chat from "../models/Chat";
+import User from "../models/User";
 
 const getChats = async (req, res) => {
   try {
     const bs_token = req.query.bs_token;
+
+    const user = await User.findOne({ bs_token });
+    const user_id = user?._id;
+
     const chats = await Chat.find({
-      $or: [{ creator_id: bs_token }, { participants: bs_token }],
-    });
+      $or: [{ creator_id: user_id }, { participants: { $in: [user_id] } }],
+    })
+      .populate(["participants"])
+      .exec()
+      .then((chatsFound) => chatsFound);
 
     res.send({ chats });
   } catch (error) {
