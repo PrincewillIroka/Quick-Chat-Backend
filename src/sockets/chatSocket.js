eeb18d06e723dd56fb457d37b50bc6c89df3eb5e
Princewill Iroka <princewilliroka@outlook.com>
2023-06-07 23:16:35 +0100

    Update chatSocket

---
+++ src/sockets/chatSocket.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import Chat from "../models/Chat";
+// import { getTokenFromCookie } from "../utils";
+const ObjectIdType = mongoose.Types.ObjectId;
+
+const chatSocket = (socket) => {
+  socket.on("join", (arg) => {
+    // const bs_token = getTokenFromCookie(socket.handshake.headers);
+    const { chat_url } = arg;
+    socket.join(chat_url);
+  });
+
+  socket.on("newMessageSent", async (arg, ack) => {
+    const { chat_url, chat_id, content, sender_id } = arg;
+
+    const newMessage = {
+      content,
+      sender: sender_id,
+      createdAt: new Date(),
+      _id: new ObjectIdType(),
+    };
+
+    let updatedChat = await Chat.findByIdAndUpdate(
+      chat_id,
+      {
+        $push: {
+          messages: newMessage,
+        },
+      },
+      { new: true }
+    )
+      .populate([
+        {
+          path: "participants",
+          select: ["name", "photo"],
+        },
+        {
+          path: "messages.sender",
+          select: ["name", "photo"],
+        },
+      ])
+      .then((chat) => (chat ? chat.toJSON() : chat))
+      .catch((err) => console.error(err));
+
+    ack({ messageSent: true, updatedChat });
+    socket.broadcast.to(chat_url).emit("newMessageReceived", { updatedChat });
+  });
+};
+
+export default chatSocket;
--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,39 +1,13 @@
-
-import cookie from "cookie";
-import { v4 as uuidv4 } from "uuid";
+import chatSocket from "./chatSocket";
 
 const sockets = (io) => {
-  // called during the socket handshake
-  io.engine.on("initial_headers", (headers, request) => {
-    headers["set-cookie"] = handleCookie(request.headers);
-  });
-
+  
   io.on("connection", (socket) => {
-    socket.handshake.headers["set-cookie"] = handleCookie(
-      socket.handshake.headers,
-      true
-    );
+    console.log("User connected successfully via socket!");
+
+    //socket handlers
+    chatSocket(socket);
   });
-};
-
-
-//Todo: Move this to util function
-const handleCookie = (headers, isConnectionEvent) => {
-  const { cookie: cookieHeaders } = headers;
-
-  let { bs_token } = cookieHeaders ? cookie.parse(cookieHeaders) : {};
-
-  if (!bs_token) {
-    const uidv4 = uuidv4();
-    bs_token = `${uidv4}_${Date.now()}`;
-  }
-
-  if (isConnectionEvent) {
-    // console.log(bs_token);
-    console.log("a user connected");
-  }
-
-  return cookie.serialize("bs_token", bs_token);
 };
 
 export default sockets;
