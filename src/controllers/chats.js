d62ba8bd2e89634b53ad391acbd4c396f49c8fc0
Princewill Iroka <princewilliroka@outlook.com>
2023-06-12 00:35:37 +0100

    Implement createChat functionality

--- src/controllers/chats.js
+++ src/controllers/chats.js
@@ -3,12 +3,24 @@ import Chat from "../models/Chat";
 
 const createChat = async (req, res) => {
   try {
-    const bs_token = req.body.bs_token;
-    const chat_url = generateChatUrl();
+    const { creator_id, passcode, chat_name } = req.body;
+    const chat_url = generateChatUrl(); //Todo: Ensure chat_url is unique from others in the db Chat model
 
-    //Todo: Ensure chat_url is unique from others in the db Chat model
+    const participants = [creator_id];
 
-    const newChat = await Chat.create({ creator_id: bs_token, chat_url });
+    let newChat = await Chat.create({
+      creator_id,
+      chat_url,
+      passcode,
+      participants,
+      chat_name,
+    });
+    newChat = await newChat.populate([
+      {
+        path: "participants",
+        select: ["name", "photo"],
+      },
+    ]);
     res.send({ newChat });
   } catch (error) {
     console.error(error);
