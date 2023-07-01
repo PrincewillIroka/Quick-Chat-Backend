543ae9dcf4bccb63760c4782bbab0b98c1017c44
Princewill Iroka <princewilliroka@outlook.com>
2023-07-01 19:35:14 +0100

    Added App health route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ import chats from "./chats";
 
 const router = express.Router();
 
+router.get("/health", async (req, res) => {
+  res.send({ success: true, message: "App is running..." });
+});
 router.use("/users", users);
 router.use("/chats", chats);
 
