6594eababb2ef47a4b04bfb5394811e367144bad
Princewill Iroka <princewilliroka@outlook.com>
2024-03-25 22:52:26 +0100

    Implemented ChatGPT for QuickChat bot

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+import config from "../config";
+
+export const CHAT_BOT_SETUP_MESSAGES = [
+  "Hello, welcome to QuickChat app.",
+  "Click on Start new conversation to create a new chat.",
+  "Then copy the chat link, and send to anyone you want to chat with.",
+  "",
+];
+
+export const GPT_PARAMETERS = [
+  {
+    role: "system",
+    content: "You are a chatbot that can chat to multiple users.",
+  },
+  {
+    role: "user",
+    content: "",
+  },
+];
