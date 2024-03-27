a75d05bcb3b406e9805c070650f22b1d9c6eb025
Princewill Iroka <princewilliroka@outlook.com>
2024-03-27 19:35:29 +0100

    Toggled rateLimitMiddleware on

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-// app.use(rateLimitMiddleware);
+app.use(rateLimitMiddleware);
 
 app.use("/api", router);
 app.use("/api/assets", express.static(path.join(__dirname, "../assets")));
