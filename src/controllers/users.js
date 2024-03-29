d9ce132baa50ee75c3840fbd1e4e28656aa01811
Princewill Iroka <princewilliroka@outlook.com>
2024-03-30 00:15:31 +0100

    Added participant-profile-updated event

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -212,9 +212,21 @@ const updateUser = async (req, res) => {
         },
       },
       { new: true }
-    );
+    ).lean();
 
-    res.send({ user: updatedUser });
+    let {
+      bs_token,
+      totalGPTMessagesReceived,
+      totalSizeOfFilesUploaded,
+      createdAt,
+      updatedAt,
+      ...rest
+    } = updatedUser;
+
+    // When a user updates their name/photo, other participants should be notified.
+    req.io.emit("participant-profile-updated", { updatedParticipant: rest });
+
+    res.send({ success: true, updatedUser: rest });
   } catch (error) {
     console.error(error);
     res.status(500);
